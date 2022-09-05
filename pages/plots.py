2ec83f0025eda1a7e9977f12355815c147367442
rohun <riyer@panynj.gov>
2022-09-05 17:16:11 -0400

    Fixing requirements

--- pages/plots.py
+++ pages/plots.py
@@ -1,5 +1,5 @@
-from email.policy import default
-from turtle import onclick
+# from email.policy import default
+# from turtle import onclick
 import streamlit as st
 import pandas as pd
 import numpy as np
